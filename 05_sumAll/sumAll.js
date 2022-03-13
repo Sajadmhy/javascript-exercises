88912e0155839778588b59e39c0e99ab7059910f
Sajadmhy <sajad.mahyaei@gmail.com>
2022-03-13 16:24:14 -0400

    Add my solution to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    let sum = 0;
+    if (n1<0 || n2<0 || isNaN(n1) || isNaN(n2)) {
+        return 'ERROR';
+    }  else if ((typeof n1 != "number") || (typeof n2 != "number")) {
+        return 'ERROR';
+    }  else if (n2>n1) {
+        for (i=n1; i<=n2; i++) {
+        sum += i;
+    }} else if (n1>n2) {
+        for (i=n2; i<=n1; i++){
+            sum += i;
+        }};
+    return sum;
 };
 
 // Do not edit below this line
