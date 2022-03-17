37d6f9a6f7e43454a439a53acc2c1dd0dfe0904e
Sajadmhy <sajad.mahyaei@gmail.com>
2022-03-17 02:02:26 -0400

    add my solution to calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	summation = a.reduce((partialSum, b) => partialSum + b, 0);
+  return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  var sum=1;
+  for (var i=0; i<a.length; i++) {
+      sum = sum * a[i];
+  } 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n == 0 || n == 1){
+    return 1;
+  }else{
+    return n * factorial(n-1);
+  };
 };
 
 // Do not edit below this line
