e2f19d4082c420484054f63fccf8ad72c0f45846
Sajadmhy <sajad.mahyaei@gmail.com>
2022-03-17 03:32:58 -0400

    Add my solution to fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n>=0) {
+        let fib = [0, 1];
+        let data = [];
+        
+        for(let i = 2; i <= 30; i++) {
+          fib[i] = fib[i - 1] + fib[i - 2]; 
+          data.push(fib[i]);
+        };
+        return fib[n]}
+        else {
+            return 'OOPS';
+        };
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
